5ee52d1e12971ab6dea534b6c521312ce6a32190
zahraejaz77 <136579856+zahraejaz77@users.noreply.github.com>
2024-07-25 15:20:08 +0500

    contact form fixes

--- assets/mail/contact-form.php
+++ assets/mail/contact-form.php
@@ -1,4 +1,5 @@
 <?php
+// Get form data
 $conName = $_POST['conName'];
 $conLName = $_POST['conLName'];
 $conEmail = $_POST['conEmail'];
@@ -6,44 +7,36 @@ $conPhone = $_POST['conPhone'];
 $conService = $_POST['conService'];
 $conMessage = $_POST['conMessage'];
 
+// Enable error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-/**
- * Set the recipient email address.
- * 
- * FIXME: Update this to your desired email address.
- */
+// Set the recipient email address
 $recipient = "zahraejaz77@gmail.com";
 
-// Set the email subject.
-$sender = $conName . " { " . $conEmail . " }";
+// Set the email subject
+$subject = "New message from your website contact form";
 
+// Email Header
+$head = "You have a new message from your website Contact Form\n=============================";
 
-//Email Header
-$head = "You have a new message from your Gerold website Contact Form\n=============================";
-
-// Build the email content.
+// Build the email content
 $form_content = "$head\n\n";
+$form_content = "Full Name: $conName $conLName\n";
+$form_content = "Email: $conEmail\n";
+$form_content = "Phone: $conPhone\n";
+$form_content = "Service: $conService\n";
+$form_content = "Message: \n$conMessage\n";
 
-$form_content .= "Full Name: $conName $conLName\n";
-
-$form_content .= "Email: $conEmail\n";
-
-$form_content .= "Phone: $conPhone\n";
-
-$form_content .= "Service: $conService\n";
-
-$form_content .= "Message: \n$conMessage\n";
-
-
-// Build the email headers.
-$headers = "From: $conName < $conEmail >\r\n" .
-  "Reply-To:" . $conEmail . "\r\n" .
+// Build the email headers
+$headers = "From: $conName <$conEmail>\r\n" .
+  "Reply-To: $conEmail\r\n" .
   'X-Mailer: PHP/' . phpversion();
 
-if (mail($recipient, $sender, $form_content, $headers)) {
+// Send the email
+if (mail($recipient, $subject, $form_content, $headers)) {
   echo "Y";
 } else {
   echo "N";
 }
+?>
